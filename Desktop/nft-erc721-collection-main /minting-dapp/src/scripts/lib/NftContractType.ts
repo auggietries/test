fbfaecc9f643836d2f1abb0a618226ab6ad80318
macbookair <macbookair@macbookairs-MacBook-Air.local>
2022-04-11 00:17:01 +0800

    Add existing file

---
+++ Desktop/nft-erc721-collection-main /minting-dapp/src/scripts/lib/NftContractType.ts
@@ -0,0 +1,7 @@
+// The name below ("TestBaby") should match the name of your Solidity contract.
+// It can be updated using the following command:
+// yarn rename-contract NEW_CONTRACT_NAME
+// Please DO NOT change it manually!
+import { TestBaby as NftContractType } from '../../../../smart-contract/typechain/index';
+
+export default NftContractType;
